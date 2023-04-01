a617699a68b6b6316e358875b97727026a333394
doudou <edouard.talma@gmail.com>
2023-04-01 16:06:04 -0400

    Testing chatGpt to improve ContinentController

--- src/main/kotlin/com/doutoutdou/aotw/kotlin/controller/ContinentController.kt
+++ src/main/kotlin/com/doutoutdou/aotw/kotlin/controller/ContinentController.kt
@@ -11,17 +11,38 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/continents")
 class ContinentController {
+//
+//    @GetMapping
+//    fun getContinents() : ResponseEntity<List<Continent>> {
+//        var c1 = Continent("continent1")
+//        var c2 = Continent("continent2")
+//        return ResponseEntity.ok(listOf(c1, c2));
+//    }
+//    @GetMapping("/{id}")
+//    fun getContinent(@PathVariable id:String) : ResponseEntity<Continent> {
+//
+//        return ResponseEntity.ok(Continent("name$id"));
+//
+//    }
+
+
+    private val continents = mapOf(
+        "1" to Continent("continent1"),
+        "2" to Continent("continent2")
+    )
 
     @GetMapping
-    fun getContinents() : ResponseEntity<List<Continent>> {
-        var c1 = Continent("continent1")
-        var c2 = Continent("continent2")
-        return ResponseEntity.ok(listOf(c1, c2));
+    fun getContinents(): ResponseEntity<List<Continent>> {
+        return ResponseEntity.ok(continents.values.toList())
     }
+
     @GetMapping("/{id}")
-    fun getContinent(@PathVariable id:String) : ResponseEntity<Continent> {
-
-        return ResponseEntity.ok(Continent("name$id"));
-
+    fun getContinent(@PathVariable id: String): ResponseEntity<Continent> {
+        val continent = continents[id]
+        return if (continent != null) {
+            ResponseEntity.ok(continent)
+        } else {
+            ResponseEntity.notFound().build()
+        }
     }
 }
