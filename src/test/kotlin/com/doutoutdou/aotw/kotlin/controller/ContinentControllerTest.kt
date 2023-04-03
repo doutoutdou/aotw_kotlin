dd0f7e7f33afbc7fc818913f46096e7b0a0bc2d6
doudou <edouard.talma@gmail.com>
2023-04-03 12:06:24 -0400

    Adding unit tests with mockK (Thanks chatGPT)

--- src/test/kotlin/com/doutoutdou/aotw/kotlin/controller/ContinentControllerTest.kt
+++ src/test/kotlin/com/doutoutdou/aotw/kotlin/controller/ContinentControllerTest.kt
@@ -3,11 +3,12 @@ package com.doutoutdou.aotw.kotlin.controller
 
 import com.doutoutdou.aotw.kotlin.dto.ContinentDto
 import com.doutoutdou.aotw.kotlin.service.ContinentService
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.Assertions.assertNull
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.Mock
 import org.springframework.http.HttpStatus
 
 class ContinentEntityDtoControllerTests {
@@ -16,30 +17,60 @@ class ContinentEntityDtoControllerTests {
         "2" to ContinentDto("continent2")
     )
 
-//    private val controller = ContinentController()
+    private lateinit var controller: ContinentController
+    private lateinit var service: ContinentService
 
-//    @Test
-//    fun `getContinents returns list of continents`() {
-//        val result = controller.getContinents()
-//        assertEquals(HttpStatus.OK, result.statusCode)
-//        assertNotNull(result.body)
-//        assertEquals(2, result.body?.size)
-//        assertEquals("continent1", result.body?.get(0)?.name)
-//        assertEquals("continent2", result.body?.get(1)?.name)
-//    }
-//
-//    @Test
-//    fun `getContinent returns continent by ID`() {
-//        val result = controller.getContinent("1")
-//        assertEquals(HttpStatus.OK, result.statusCode)
-//        assertNotNull(result.body)
-//        assertEquals("continent1", result.body?.name)
-//    }
-//
-//    @Test
-//    fun `getContinent returns 404 for non-existent ID`() {
-//        val result = controller.getContinent("3")
-//        assertEquals(HttpStatus.NOT_FOUND, result.statusCode)
-//        assertNull(result.body)
-//    }
+    @BeforeEach
+    fun setup() {
+        service = mockk(relaxed = true)
+        controller = ContinentController(service)
+    }
+
+    @Test
+    fun `list continents should return a list of continents`() {
+        every { service.listContinents() } returns continents.values.toList()
+        val result = controller.getContinents()
+        assertEquals(HttpStatus.OK, result.statusCode)
+        assertNotNull(result.body)
+        assertEquals(2, result.body?.size)
+        assertEquals("continent1", result.body?.get(0)?.name)
+        assertEquals("continent2", result.body?.get(1)?.name)
+    }
+
+    @Test
+    fun `get continent should return a continent when it exists`() {
+        val id = 1L
+        val expected = ContinentDto("name1", 1)
+        every { service.getContinent(id) } returns expected
+        val result = controller.getContinent(id)
+        assertEquals(expected, result.body)
+        assertEquals(HttpStatus.OK, result.statusCode)
+    }
+
+    @Test
+    fun `get continent should return not found when it does not exist`() {
+        val id = 3L
+        every { service.getContinent(id) } returns null
+        val result = controller.getContinent(id)
+        assertNull(result.body)
+        assertEquals(HttpStatus.NOT_FOUND, result.statusCode)
+    }
+
+    @Test
+    fun `create continent should return a continent`() {
+        val dto = ContinentDto(name = "continent3")
+        val expected = ContinentDto(name = "Continent3", id = 3)
+        every { service.createContinent(dto) } returns expected
+        val result = controller.createContinent(dto)
+        assertEquals(expected, result.body)
+        assertEquals(HttpStatus.OK, result.statusCode)
+    }
+
+    @Test
+    fun `delete continent should call service`() {
+        val id = 1L
+        every { service.deleteContinent(id) } returns Unit
+        controller.deleteContinent(id)
+        verify { service.deleteContinent(any()) }
+    }
 }
